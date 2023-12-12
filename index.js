b9a15248ff0ae8f141fc8e88e67cde13a154f02a
Muhammad Fauzan Muharram <105500225+Fauzanmhr@users.noreply.github.com>
2023-12-12 08:46:23 +0700

    Update index.j: chnage domain origin to env format

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(session({
 }));
 
 
-const origin = process.env.NODE_ENV === 'production' ? 'https://yourproductiondomain.com' : 'http://localhost:3000';
+const origin = process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'http://localhost:3000';
 
 app.use(cors({
     credentials: true,
