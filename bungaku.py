b4e6a6a06b62757303a86ae38d550cf4da56e62e
fauzanmhr <fauzan432003@gmail.com>
2023-12-15 08:51:20 +0700

    fix model

--- bungaku.py
+++ bungaku.py
@@ -1,16 +1,18 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-train_data_dir = "development/dataset/train"
-validation_data_dir = "development/dataset/val"
+import matplotlib.pyplot as plt
 
 # Image dimensions
-img_width, img_height = 256, 256
+img_width, img_height = 224, 224
 
 # Batch size
 batch_size = 32
 
-# Create data generators
+# Data directories
+train_data_dir = "development/dataset/train"
+validation_data_dir = "development/dataset/val"
+
+# Create data generators with separate validation data
 train_datagen = ImageDataGenerator(
     rescale=1.0 / 255,
     shear_range=0.2,
@@ -54,16 +56,21 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(14, activation='softmax')  # Number of classes
 ])
 
-# Compile the model
+# Compile the model with learning rate scheduling
+initial_learning_rate = 0.001
+lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
+)
+
 model.compile(
-    optimizer='adam',
+    optimizer=tf.keras.optimizers.Adam(learning_rate=lr_schedule),
     loss='categorical_crossentropy',
     metrics=['accuracy'],
 )
 
 # Train the model
-epochs = 10
-model.fit(
+epochs = 7
+history = model.fit(
     train_generator,
     steps_per_epoch=train_generator.samples // batch_size,
     epochs=epochs,
@@ -71,12 +78,29 @@ model.fit(
     validation_steps=validation_generator.samples // batch_size,
 )
 
+# Plot training history
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('development/model/test/training_plot.png')
+plt.show()
+
+# Save the model in HDF5 format
+model.save('development/model/bungaku.h5')
+
 # Convert the model to TFLite format
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 # Save the TFLite model to a file
-with open('development/model/flower_model.tflite', 'wb') as f:
+with open('development/model/bungaku.tflite', 'wb') as f:
     f.write(tflite_model)
 
-print("TFLite model saved successfully!")
+# Save model architecture summary to a text file
+with open('development/model/model_summary.txt', 'w') as f:
+    model.summary(print_fn=lambda x: f.write(x + '\n'))
+
+print("Model, TFLite model, plot, and model architecture summary saved successfully!")
